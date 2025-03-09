03b95dfed2e5c24c2ea2dd777c885abc42a3a7cf
새미 <139088277+saemii-24@users.noreply.github.com>
2025-03-09 15:30:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.81 ms
+메모리: 33.5 MB, 시간: 1.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 26일 19:15:46
+2025년 03월 09일 15:30:55
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,11 +1,14 @@
 function solution(numbers) {
-    let result = new Set();
+    // (중복값 제거를 위함)
+    let mySet = new Set();
     
-    for (let i = 0; i < numbers.length - 1; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            result.add(numbers[i] + numbers[j]);
-        }
+    //1. 서로 다른 인덱스에 있는 두 개의 수를 뽑고 더한다.
+    for(let a = 0; a < numbers.length; a++){
+        for(let b = 0; b < numbers.length; b++){
+            //2. 이 때 나올 수 있는 숫자의 중복값을 제거한다.
+            if(a !== b) mySet.add(numbers[a]+numbers[b])
+        }   
     }
-
-    return [...result].sort((a, b) => a - b);
+    //3. 모든 값을 오름차순으로 정렬하여 return한다.
+    return [...mySet].sort((a,b)=>a-b)
 }
