9a83461e345b3ed82797f09855d80c2ddc5bec57
새미 <139088277+saemii-24@users.noreply.github.com>
2025-03-17 19:35:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.31 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,9 @@
 function solution(participant, completion) {
-    const obj = {};
-    
-    for (let part of participant) {
-        obj[part] = (obj[part] || 0) + 1;
+    let obj = {};
+    for(let part of participant) obj[part] = (obj[part] || 0) + 1;
+    for(let comp of completion){
+         obj[comp] = obj[comp] - 1
+        if(obj[comp] === 0) delete obj[comp]
     }
-    
-    for (let comp of completion) {
-        if (obj[comp]) {
-            obj[comp] -= 1;
-            if (obj[comp] === 0) {
-                delete obj[comp];
-            }
-        }
-    }
-    
-    return Object.keys(obj)[0];
+    return  Object.entries(obj)[0][0];
 }
