abf311e8a689dee36c7bc62726ba1007e720b134
새미 <139088277+saemii-24@users.noreply.github.com>
2025-03-25 20:55:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 206.87 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,40 +1,32 @@
 function solution(id_list, report, k) {
-    let user = {};
-    let myrep = {};
+  let 신고당한횟수 = {};
+  let 누가신고했나 = {};
 
-    for (let id of id_list) {
-        user[id] = {};
-        myrep[id] = {};
+  id_list.forEach((user) => {
+    신고당한횟수[user] = 0;
+    누가신고했나[user] = new Set();
+  });
+
+  for (let rep of report) {
+    let [신고자, 신고된사람] = rep.split(" ");
+    if (!누가신고했나[신고자].has(신고된사람)) {
+      누가신고했나[신고자].add(신고된사람);
+      신고당한횟수[신고된사람] += 1; //동일인물이 신고한 횟수는 1로 카운팅
     }
+  }
 
-    let rep = {};
-    for (let reportUser of report) {
-        let [me, you] = reportUser.split(' ');
-
-        if (!user[me][you]) {
-            user[me][you] = 1;
-            myrep[you] = myrep[you] || {};
-            myrep[you][me] = 1;
-        }
+  let 정지된유저 = new Set();
+  for (let user in 신고당한횟수) {
+    if (신고당한횟수[user] >= k) {
+      정지된유저.add(user);
     }
+  }
 
-    for (let key of Object.keys(myrep)) {
-        rep[key] = Object.keys(myrep[key]).length;
-    }
-
-    let stop = [];
-    for (let key of Object.keys(rep)) {
-        if (rep[key] >= k) stop.push(key);
-    }
-
-    let result = [];
-    for (let stopUser of id_list) {
-        let cnt = 0;
-        for (let countUser of stop) {
-            if (user[stopUser][countUser]) cnt++;
-        }
-        result.push(cnt);
-    }
-
-    return result;
+  return id_list.map((user) => {
+    let count = 0;
+    누가신고했나[user].forEach((신고한유저) => {
+      if (정지된유저.has(신고한유저)) count++;
+    });
+    return count;
+  });
 }
