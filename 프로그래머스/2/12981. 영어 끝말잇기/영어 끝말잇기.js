8528d1bad76bf15212024869b0bf61cf3cbc8bca
새미 <139088277+saemii-24@users.noreply.github.com>
2025-03-31 21:21:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,18 +1,27 @@
 function solution(n, words) {
-    let obj = {}; 
-    let prev = "";
+    let mySet = new Set();
+    let turn = 1;
     
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-        let player = (i % n) + 1;
-        let turn = Math.ceil((i + 1) / n);
-
-        if (obj[word]) return [player, turn];
-        if (i > 0 && word[0] !== prev) return [player, turn];
-
-        obj[word] = true;
-        prev = word[word.length - 1];
+    // 첫번째 마지막 알파벳
+    let last = words[0][words[0].length-1];
+    
+    // 첫번째 단어는 항상 통과하므로 1부터 시작
+    mySet.add(words[0]);
+    
+    for (let i = 1; i < words.length; i++) {
+        // 끝말잇기 실패 1. 첫 글자가 마지막 알파벳과 다를 때
+        if (words[i][0] !== last) {
+            return [i % n + 1, Math.ceil((i + 1) / n)];
+        } else {
+            last = words[i][words[i].length - 1];
+        }
+        
+        // 끝말잇기 실패 2. 이미 말한 단어를 반복했을 때
+        if (mySet.has(words[i])) {
+            return [i % n + 1, Math.ceil((i + 1) / n)];
+        } else {
+            mySet.add(words[i]);
+        }
     }
-
     return [0, 0];
 }
